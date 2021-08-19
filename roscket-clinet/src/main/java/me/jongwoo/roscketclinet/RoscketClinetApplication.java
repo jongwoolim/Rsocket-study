c2dfa84e0d85216f49a1e64e1b7026326ff7a32c
limjongwoo <whddn528@naver.com>
2021-08-19 14:29:08 +0900

    rsocket-clinet init

---
+++ roscket-clinet/src/main/java/me/jongwoo/roscketclinet/RoscketClinetApplication.java
@@ -0,0 +1,13 @@
+package me.jongwoo.roscketclinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoscketClinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoscketClinetApplication.class, args);
+    }
+
+}
---
+++ roscket-clinet/src/test/java/me/jongwoo/roscketclinet/RoscketClinetApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jongwoo.roscketclinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoscketClinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
