9195a7f9bdc4174fba9b1dbf1e21f113e2cfef55
limjongwoo <whddn528@naver.com>
2021-08-19 20:01:39 +0900

    findItemsUsingRScoketRequestStream add

--- roscket-clinet/src/main/java/me/jongwoo/roscketclinet/controller/RScoketController.java
+++ roscket-clinet/src/main/java/me/jongwoo/roscketclinet/controller/RScoketController.java
@@ -1,18 +1,21 @@
 package me.jongwoo.roscketclinet.controller;
 
 import me.jongwoo.roscketclinet.domain.Item;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.time.Duration;
 
 import static io.rsocket.metadata.WellKnownMimeType.MESSAGE_RSOCKET_ROUTING;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.parseMediaType;
+import static org.springframework.http.MediaType.*;
 
 @RestController
 public class RScoketController {
@@ -37,5 +40,14 @@ public class RScoketController {
                     .retrieveMono(Item.class))
                 .map(savedItem -> ResponseEntity.created(
                         URI.create("/items/request-response")).body(savedItem));
+    }
+
+    @GetMapping(value = "/items/request-stream", produces = APPLICATION_NDJSON_VALUE)
+    public Flux<Item> findItemsUsingRScoketRequestStream(){
+        return this.requester
+                .flatMapMany(rSocketRequester -> rSocketRequester
+                    .route("/newItems.request-stream")
+                    .retrieveFlux(Item.class)
+                    .delayElements(Duration.ofSeconds(1)));
     }
 }
