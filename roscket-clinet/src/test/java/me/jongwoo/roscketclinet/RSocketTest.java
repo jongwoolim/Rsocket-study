5e3442d0e419b152f67ab9537d61986f8ba021cc
limjongwoo <whddn528@naver.com>
2021-08-19 20:46:29 +0900

    addNewItemUsingRSocketFireAndForget add

--- roscket-clinet/src/test/java/me/jongwoo/roscketclinet/RSocketTest.java
+++ roscket-clinet/src/test/java/me/jongwoo/roscketclinet/RSocketTest.java
@@ -6,8 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,6 +54,71 @@ public class RSocketTest {
         this.repository.findAll()
                 .as(StepVerifier::create)
                 .expectNextMatches(item -> {
+                    assertThat(item.getId()).isNotNull();
+                    assertThat(item.getName()).isEqualTo("Alf alarm clock");
+                    assertThat(item.getDescription()).isEqualTo("nothing important");
+                    assertThat(item.getPrice()).isEqualTo(19.99);
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    public void verifyRemoteOperationsThroughRSocketRequestStream(){
+
+        this.repository.deleteAll().block();
+
+
+        List<Item> items = IntStream.rangeClosed(1, 3)
+                .mapToObj(i -> new Item("name - " + i, "description - " + i, i))
+                .collect(Collectors.toList());
+
+        this.repository.saveAll(items).blockLast();
+
+        this.webTestClient.get().uri("/items/request-stream")
+                .accept(MediaType.APPLICATION_NDJSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Item.class)
+                .getResponseBody()
+                .as(StepVerifier::create)
+                .expectNextMatches(itemPredicate("1"))
+                .expectNextMatches(itemPredicate("2"))
+                .expectNextMatches(itemPredicate("3"))
+                .verifyComplete();
+    }
+
+    private Predicate<Item> itemPredicate(String num) {
+        return item -> {
+            assertThat(item.getName()).startsWith("name");
+            assertThat(item.getName()).endsWith(num);
+            assertThat(item.getDescription()).startsWith("description");
+            assertThat(item.getDescription()).endsWith(num);
+            assertThat(item.getPrice()).isPositive();
+            return true;
+        };
+    }
+
+    @Test
+    public void verifyRemoteOperationsThroughRSocketFireAndForget() throws InterruptedException {
+        // 데이터 초기화
+        this.repository.deleteAll()
+                .as(StepVerifier::create)
+                .verifyComplete();
+
+        // 새 Item 생성
+        this.webTestClient.post().uri("/items/fire-and-forget")
+                .bodyValue(new Item("Alf alarm clock", "nothing important", 19.99))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().isEmpty();
+
+        Thread.sleep(500);
+
+        // Item 몽고디비에 저장됐는지 확인
+        this.repository.findAll()
+                .as(StepVerifier::create)
+                .expectNextMatches(item ->{
                     assertThat(item.getId()).isNotNull();
                     assertThat(item.getName()).isEqualTo("Alf alarm clock");
                     assertThat(item.getDescription()).isEqualTo("nothing important");
